3f21e2a3065eff4ceec7805cb68980e6aeca2833
Joe Luo <sonicjoy2002@hotmail.com>
2015-08-20 23:00:09 +0100

    Removed unused references

--- WebLoginViewer/WebLoginViewTest/MainFunctionTest.cs
+++ WebLoginViewer/WebLoginViewTest/MainFunctionTest.cs
@@ -1,6 +1,5 @@
-﻿using WebLoginViewer;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Collections.Generic;
 using System.Linq;
 
