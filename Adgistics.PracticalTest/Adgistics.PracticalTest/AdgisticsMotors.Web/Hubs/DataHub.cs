1c324e9148ba67ecbba83d4fb8c14956e45b7392
Joe Luo <sonicjoy2002@hotmail.com>
2015-09-10 22:06:27 +0100

    Added web interface

---
+++ Adgistics.PracticalTest/Adgistics.PracticalTest/AdgisticsMotors.Web/Hubs/DataHub.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace AdgisticsMotors.Web.Hubs
+{
+    public class DataHub : Hub
+    {
+        public void SendProgress(string name, string message)
+        {
+            // Call the addNewMessageToPage method to update clients.
+            Clients.Caller.addNewMessageToPage(name, message);
+        }
+    }
+}
---
+++ Adgistics.PracticalTest/Adgistics.PracticalTest/AdgisticsMotors.Web/Hubs/Startup.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(AdgisticsMotors.Web.Hubs.Startup))]
+
+namespace AdgisticsMotors.Web.Hubs
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.MapSignalR();
+        }
+    }
+}
